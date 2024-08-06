e8b25d107a054920156d27c1eee6e79f7a25e144
Qiydaarwilliams23@gmail.com <Qiydaarwilliams23@gmail.com>
2024-08-06 16:41:30 -0400

    add form created

--- taskmate/todolist_app/views.py
+++ taskmate/todolist_app/views.py
@@ -1,12 +1,18 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import TaskList
+from .form import TaskForm
 
 
 def todolist(request):
-    all_tasks = TaskList.objects.all()
- 
-    return render(request, 'todolist.html', {'all_tasks': all_tasks})
+    if request.method =="POST":
+        form = TaskForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+        return redirect('todolist')    
+    else:    
+        all_tasks = TaskList.objects.all()
+        return render(request, 'todolist.html', {'all_tasks': all_tasks})
 
 def contact(request):
     context = {
